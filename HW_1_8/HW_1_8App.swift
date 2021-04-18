3f88e4187792437529caf03b0def91f982af57f4
Pavel Sakhanko <iospaveldev@gmail.com>
2021-04-18 20:24:25 +0200

    Initial Commit

---
+++ HW_1_8/HW_1_8App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1_8App.swift
+//  HW_1_8
+//
+//  Created by Pavel Sakhanko on 18.04.21.
+//
+
+import SwiftUI
+
+@main
+struct HW_1_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
